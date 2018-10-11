84236c32f5c23a674e4e1942bd5eda0e9ab31b21
Arman Mussagaliyev <arman.mussagaliyev@hireright.com>
2018-10-11 12:20:12 +0300

    refactoring to multimodule maven project. closes #1

--- pet-clinic-web/src/main/java/guru/springframework/sfgpetclinic/SfgPetClinicApplication.java
+++ pet-clinic-web/src/main/java/guru/springframework/sfgpetclinic/SfgPetClinicApplication.java
@@ -1,5 +1,11 @@
 package guru.springframework.sfgpetclinic;
 
+/*
+ * Copyright 2001-2018 by HireRight, Inc. All rights reserved.
+ * This software is the confidential and proprietary information
+ * of HireRight, Inc. Use is subject to license terms.
+ */
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
